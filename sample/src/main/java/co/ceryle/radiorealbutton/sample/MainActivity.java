54ca6bdbbd3faffd57172546c6fa8c145c73b4d8
Alexander Kurchatov <kurchatov.work@gmail.com>
2018-10-15 06:03:38 +0800

    Gradle

--- sample/src/main/java/co/ceryle/radiorealbutton/sample/MainActivity.java
+++ sample/src/main/java/co/ceryle/radiorealbutton/sample/MainActivity.java
@@ -15,7 +15,7 @@
  */
 package co.ceryle.radiorealbutton.sample;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
